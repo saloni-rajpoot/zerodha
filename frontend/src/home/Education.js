279de0a0d59058ded32d2e0d031ab6b678395f38
saloni-rajpoot <salonithakur7968@gmail.com>
2026-07-17 10:09:50 +0530

    Added home component and updated project

---
+++ frontend/src/home/Education.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Education() {
+    return ( 
+        <h1>
+            Eduacation
+        </h1>
+     );
+}
+
+export default Education;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import HomePage from './home/HomePage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <div>
-    <h1>Hello</h1>
-   </div>
+      <HomePage />
   </React.StrictMode>
 );
-
+  
+   
