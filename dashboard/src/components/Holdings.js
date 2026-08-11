bfc1de08445c923832280bc52634675e1f5cb2a8
saloni-rajpoot <salonithakur7968@gmail.com>
2026-08-11 18:52:24 +0530

    connecting dashboard with backend

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import {holdings} from "../data/data";
-function Holdings() {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// import {holdings} from "../data/data";
+const Holdings = () => {
+  const [allHoldings, setAllHoldings] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allHoldings").then((res) => {
+      // console.log(res.data);
+      setAllHoldings(res.data);
+    });
+  }, []);
+
+
     return (
     <>
-    <h3 className="title">Holdings ({holdings.length})</h3>
+    <h3 className="title">Holdings ({allHoldings.length})</h3>
       <div className="order-table">
         <table>
           <tr>
@@ -17,7 +28,7 @@ function Holdings() {
             <th>Day chg.</th>
           </tr>
 
-          {holdings.map((stock, index) => {
+          {allHoldings.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
